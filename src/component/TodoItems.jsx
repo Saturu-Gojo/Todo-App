188b4a1794b2d9ecf88f7ed0822935b8c43c2409
Saturu-Gojo <prashasth068@gmail.com>
2026-03-15 07:42:43 +0530

    Updated Code

--- src/component/TodoItems.jsx
+++ src/component/TodoItems.jsx
@@ -1,22 +1,18 @@
-import { TodoItemsContext } from '../Store/TodoitemsContext.jsx';
-import TodoItem from './TodoItem.jsx'
-import {useContext} from "react"
-
-
+import { TodoItemsContext } from "../store/TodoItemsContext";
+import TodoItem from "./TodoItem";
+import {useContext} from "react";
 
 const TodoItems = () => {
 
-  const {todos} = useContext(TodoItemsContext);
-    
+  const {todoItems} = useContext(TodoItemsContext);
 
-    return (
-        <>
-        {todos.map((item) => (
-          <TodoItem key={item.id} id={item.id} todoText={item.todoText} todoDate={item.todoDate}
-           />
-        ))}
-        </>
-    );
-  };
-  
-  export default TodoItems;
+  return (
+    <>
+      {todoItems.map((item) => (
+        <TodoItem key={item.id} id={item.id} todoText={item.todoText} todoDate={item.todoDate}/>
+      ))}
+    </>
+  );
+};
+
+export default TodoItems;
